d8a74aa81f6f5260fea827a6eccdf6df15e4fd6a
cvazzz <sebastian_159@hotmail.es>
2023-12-31 15:31:09 -0500

    Se agrega la practica

---
+++ scripts.js
@@ -0,0 +1,49 @@
+function ShowHide(){
+    var container = document.getElementsByClassName("container-1")[0];
+
+    if(container.style.visibility=="hidden"){
+        container.style.visibility = "visible";
+    }else{
+        container.style.visibility = "hidden";
+    }
+}
+
+function ShowHid(){
+    var container = document.getElementsByClassName("container-2")[0];
+
+    if(container.style.visibility=="hidden"){
+        container.style.visibility = "visible";
+    }else{
+        container.style.visibility = "hidden";
+    }
+}
+
+function ShowHi(){
+    var container = document.getElementsByClassName("container-3")[0];
+
+    if(container.style.visibility=="hidden"){
+        container.style.visibility = "visible";
+    }else{
+        container.style.visibility = "hidden";
+    }
+}
+
+function ShowH(){
+    var container = document.getElementsByClassName("container-4")[0];
+
+    if(container.style.visibility=="hidden"){
+        container.style.visibility = "visible";
+    }else{
+        container.style.visibility = "hidden";
+    }
+}
+
+function Show(){
+    var container = document.getElementsByClassName("container-5")[0];
+
+    if(container.style.visibility=="hidden"){
+        container.style.visibility = "visible";
+    }else{
+        container.style.visibility = "hidden";
+    }
+}
